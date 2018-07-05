e1fd9c124f771c8abdb9f4130a574b4beeeb81d6
Emilio <ec@ekio.jp>
2018-07-05 15:13:53 +0900

    Adding interface Standby

--- validator.py
+++ validator.py
@@ -17,41 +17,64 @@ from common import login
 from common import grep_all
 
 __author__ = 'Emilio <ec@ekio.jp>'
-__date__ = 'Jun-21-2018'
-__version__ = '0.3'
+__date__ = 'Jul-5-2018'
+__version__ = '0.4'
 
 def device_session(IP,scope,gu,gp,ju,jp,tu,tp,te,ku,kp,ke,output_q):
     output_dict = {}
     net_connect = login(IP,gu,gp,ju,jp,tu,tp,te,ku,kp,ke)
     if net_connect:
-        prompt = re.sub('[>|#]','',net_connect.find_prompt())
-        shint = net_connect.send_command('show ip interface brief | i '+IP).split('\n')
+        prompt = re.sub('[>|#]', '', net_connect.find_prompt())
+        shint = net_connect.send_command('show ip interface brief | i ' + IP).split('\n')
         if len(shint) > 1:
             for x in range(len(shint)):
                 if shint[x].split()[1] == IP:
-                    shrunint = net_connect.send_command('show run interface '+shint[x].split()[0]+' | i helper-address|dhcp relay address')
+                    shrunint = net_connect.send_command('show run interface '
+                                                        + shint[x].split()[0]
+                                                        + ' | i helper-address|dhcp relay address')
                     if re.search('helper-address', shrunint):
-                        output = ',IOS,'+prompt+','+IP+','+shint[x].split()[0]+',None\n'
+                        output = ',IOS,' + prompt + ',' + IP + ',' + shint[x].split()[0]
+                        + ',None,None,None\n'
                     elif re.search('dhcp relay address', shrunint):
-                        output = ',NX,'+prompt+','+IP+','+shint[x].split()[0]+',None\n'
+                        output = ',NX,' + prompt + ',' + IP + ',' + shint[x].split()[0]
+                        + ',None,None,None\n'
                     else:
-                        output = ',None,'+prompt+','+IP+','+shint[x].split()[0]+',None\n'
+                        output = ',None,' + prompt + ',' + IP + ',' + shint[x].split()[0]
+                        + ',None,None,None\n'
         else:
-            shstb = net_connect.send_command('show standby brief | i '+IP)
+            shstb = net_connect.send_command('show standby brief | i ' + IP)
             if 'Invalid' not in shstb:
-                m = re.search('.* '+IP,shstb)
+                m = re.search('.* ' + IP, shstb)
                 if m:
-                    peerip = re.search('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',shstb).group()
-                    peer_connect = login(peerip,gu,gp,ju,jp,tu,tp,te,ku,kp,ke)
+                    regex = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+                    peerip = re.search(regex, shstb).group()
+                    peer_connect = login(peerip,
+                                         gu,
+                                         gp,
+                                         ju,
+                                         jp,
+                                         tu,
+                                         tp,
+                                         te,
+                                         ku,
+                                         kp,
+                                         ke
+                                        )
                     if peer_connect:
-                        peer_prompt = re.sub('[>|#]','',peer_connect.find_prompt())
+                        peer_prompt = re.sub('[>|#]', '', peer_connect.find_prompt())
+                        shint = peer_connect.send_command('show ip interface brief | i ' + peerip).split('\n')
+                        if len(shint) == 1:
+                            peerint = shint[0].split()[0]
+                        else:
+                            peerint = 'None'
                         peer_connect.disconnect()
                     else:
-                        peer_prompot = 'None'
-                    output = ',IOS,'+prompt+','+IP+','+m.group().split()[0]+','+peer_prompt+','+peerip+'\n'
+                        peer_prompot = 'None,None,None'
+                    output = (',IOS,' + prompt + ',' + IP + ',' + m.group().split()[0] + ','
+                              + peer_prompt + ',' + peerip + ',' + peerint + '\n')
             else:
-                shhsrp = net_connect.send_command('show hsrp brief | i '+IP)
-                m = re.search('.* '+IP,shhsrp)
+                shhsrp = net_connect.send_command('show hsrp brief | i ' + IP)
+                m = re.search('.* ' + IP, shhsrp)
                 if m:
                     regex = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
                     peerip = re.search(regex, shhsrp).group()
@@ -69,16 +92,20 @@ def device_session(IP,scope,gu,gp,ju,jp,tu,tp,te,ku,kp,ke,output_q):
                                         )
                     if peer_connect:
                         peer_prompt = re.sub('[>|#]', '' , peer_connect.find_prompt())
+                        shint = peer_connect.send_command('show ip interface brief | i ' + peerip).split('\n')
+                        if len(shint) == 1:
+                            peerint = shint[0].split()[0]
+                        else:
+                            peerint = 'None'
                         peer_connect.disconnect()
                     else:
-                        peer_prompt = 'None'
-                    output = (',NX,' + prompt + ',' + IP + ','
-                              + m.group().split()[0] + ',' + peer_prompt
-                              + ',' + peerip + '\n')
+                        peer_prompt = 'None,None,None'
+                    output = (',NX,' + prompt + ',' + IP + ',' + m.group().split()[0]
+                              + ',' + peer_prompt + ',' + peerip + ',' + peerint + '\n')
 
         net_connect.disconnect()
     else:
-        output = ',NA,NA,'+IP+',NA,NA,NA\n'
+        output = ',NA,NA,'+IP+',NA,NA,NA,NA\n'
 
     output_dict[scope] = output
     output_q.put(output_dict)
@@ -95,11 +122,11 @@ if __name__ == "__main__":
                + ' dump format (after cleaner.py)')
         print ('<scope-list> CSV output format: '
                + '<Scope>,<IOS/NX/None>,<ACT_hostname>,<ACT_IP>,'
-               + '<Interface>,<STB_hostname>,<STB_IP>\n')
+               + '<ACT_Intf>,<STB_hostname>,<STB_IP>,<STB_Intf>\n')
         sys.exit(0)
 
-    INPUT=sys.argv[1]
-    OUTPUT=sys.argv[2]
+    INPUT = sys.argv[1]
+    OUTPUT = sys.argv[2]
     salida = open(OUTPUT,'w')
 
     scopelist=[]
@@ -151,16 +178,16 @@ if __name__ == "__main__":
                     'ERROR: Scope ' + scopelist[x]
                     + ' can\'t ping scope gateway (' + IP + ')'
                 )
-                salida.write(scopelist[x]+',NotPing,,,,,\n')
+                salida.write(scopelist[x]+',NotPing,,,,,,\n')
         elif len(aver) > 1:
             print (
                 'ERROR: Scope ' + scopelist[x]
                 + ' have too many gateways'
             )
-            salida.write(scopelist[x]+',ManyGateway,,,,,\n')
+            salida.write(scopelist[x]+',ManyGateway,,,,,,\n')
         else:
             print 'ERROR: Scope ' + scopelist[x] + ' doesn\'t have a gateway'
-            salida.write(scopelist[x]+',NoGateway,,,,,\n')
+            salida.write(scopelist[x]+',NoGateway,,,,,,\n')
 
     main_thread = threading.currentThread()
     for some_thread in threading.enumerate():
